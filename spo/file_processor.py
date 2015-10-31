11e6392a9f07611049c4f5756b3a9e06dd54a560
Aarni Koskela <akx@iki.fi>
2015-10-31 21:17:08 +0200

    Fix file_processor encoding semantics

--- spo/file_processor.py
+++ spo/file_processor.py
@@ -6,13 +6,10 @@ unescape = HTMLParser.HTMLParser().unescape
 
 
 class FileProcessor(object):
-
     def __init__(self, input, output, encoding="UTF-8", **kwargs):
+        self.encoding = encoding
         self.input = codecs.EncodedFile(input, encoding)
-        if output:
-            self.output = codecs.EncodedFile(output, encoding)
-        else:
-            self.output = None
+        self.output = output
         self.fp_init(**kwargs)
 
     def fp_init(self, **kwargs):
@@ -20,8 +17,12 @@ class FileProcessor(object):
         pass
 
     def _write(self, line=""):
+        if isinstance(line, str):
+            line = line.decode(self.encoding)
+
         if self.output:
-            self.output.write("%s\n" % line)
+            line = (u"%s\n" % line).encode(self.encoding)
+            self.output.write(line)
 
     def _read_lines(self):
         for line in self.input:
