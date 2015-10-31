193671b688bc33f414d10288a022226004a84c40
Aarni Koskela <akx@iki.fi>
2015-10-31 21:09:23 +0200

    Switch to `sqlitedict` for caching

--- spo/cache.py
+++ spo/cache.py
@@ -1,41 +1,47 @@
 # -- encoding: UTF-8 --
 
 import json
-import shelve
+from sqlitedict import SqliteDict
 import os
 import string
 import time
 import unicodedata
 import pickle
+import functools
 
+def _make_key(args, kwds):
+    # h/t Python 3.4 functools
+    key = args
+    if kwds:
+        key += ("\x00\x03",)
+        for item in sorted(kwds.items()):
+            key += item
+    elif len(key) == 1 and type(key[0]) in {int, str, unicode, frozenset, type(None)}:
+        return key[0]
+    return tuple(key)
 
 class Cache(object):
 
     def __init__(self, path, max_life):
-        self.path = path
-        self.shelf = shelve.open("%s.shelve" % self.path)
+        self.storage = SqliteDict("cache.sqlite3", tablename=path)
         self.max_life = int(max_life)
 
     def transform_key(self, key):
-        key = unicode(key)
-        key = unicodedata.normalize("NFKD", key).encode("ascii", "ignore")
-        key = "".join((c if c in string.ascii_letters + string.digits else "_") for c in key)
-        key = os.path.join(self.path, key)
-        return key
+        return unicode(key)
 
     def put(self, key, value, life=0):
         expire = time.time() + (life or self.max_life)
         self._put_shelve(key, value, expire)
 
     def _put_shelve(self, key, value, expire):
-        self.shelf[self.transform_key(key)] = {"expire": expire, "value": value}
+        self.storage[self.transform_key(key)] = {"expire": expire, "value": value}
 
     def get(self, key, default=None):
         t_key = self.transform_key(key)
         expire = None
         value = default
-        if t_key in self.shelf:
-            shelved = self.shelf[t_key]
+        if t_key in self.storage:
+            shelved = self.storage[t_key]
             value = shelved["value"]
             expire = shelved["expire"]
 
@@ -45,7 +51,7 @@ class Cache(object):
         return value
 
     def has(self, key):
-        return os.path.isfile(self.transform_key(key))
+        return (self.transform_key(key) in self.storage)
 
     def __getitem__(self, item):
         return self.get(item)
@@ -55,3 +61,14 @@ class Cache(object):
 
     def __contains__(self, item):
         return self.has(item)
+
+    def cached(self, func):
+        @functools.wraps(func)
+        def cached_func(*args, **kwargs):
+            cache_key = _make_key(args, kwargs)
+            if cache_key not in self:
+                self[cache_key] = value = func(*args, **kwargs)
+            else:
+                value = self[cache_key]
+            return value
+        return cached_func
