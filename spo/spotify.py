1579261e463ea0bfcbc1bed3c02143fc7a95eefa
Aarni Koskela <akx@iki.fi>
2015-10-31 21:17:33 +0200

    Embetter Spotify API

--- spo/spotify.py
+++ spo/spotify.py
@@ -5,23 +5,33 @@ from spotipy.client import Spotify as _Spotify, SpotifyException
 
 SpotifyException = SpotifyException
 
+
 class Spotify(_Spotify):
     auth_token = None
     auth_username = os.environ.get("SPOTIFY_USERNAME")
+
     def __init__(self, auth=None):
         if auth is True:
             from spotipy.util import prompt_for_user_token
-            auth = prompt_for_user_token(self.auth_username)
+            auth = prompt_for_user_token(self.auth_username, scope=" ".join([
+                "playlist-read-private",
+                "playlist-read-collaborative",
+                "playlist-modify-public",
+                "playlist-modify-private",
+                "user-library-read",
+                "user-library-modify"
+            ]))
         if not auth:
             auth = self.auth_token
         super(Spotify, self).__init__(auth=auth)
         self.cache = Cache("spotify_cache", max_life=86400 * 7)
 
     def _cached(self, cache_key, getter):
-        value = self.cache.get(cache_key)
-        if value is None:
+        if cache_key not in self.cache:
             value = getter()
             self.cache[cache_key] = value
+        else:
+            value = self.cache[cache_key]
         return value
 
     def track(self, track_id):
@@ -29,3 +39,6 @@ class Spotify(_Spotify):
 
     def album(self, album_id):
         return self._cached("album %s" % album_id, lambda: super(Spotify, self).album(album_id))
+
+    def search(self, **kwargs):
+        return self.cache.cached(super(Spotify, self).search)(**kwargs)
